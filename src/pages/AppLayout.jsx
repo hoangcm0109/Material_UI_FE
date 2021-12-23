0dc360d0ed1bbd8e5a276a51e8af2476329cb539
Minh Hoàng <chuminhhoang19@gmail.com>
2021-12-23 16:44:24 +0700

    fix header navbar

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import { Box, colors, Toolbar } from '@mui/material'
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { Loading, TopNav } from '../components'
 
 const AppLayout = () => {
+    const [isLoading, setIsLoading] = useState(false)
+
     return (
-        <div>
-            AppLayout
-        </div>
+        isLoading ? (
+            <Box sx={{
+                width: '100%', height: '100vh'
+            }}>
+                <Loading /> 
+            </Box>
+
+        ) : (
+            <Box>
+                <TopNav />
+                <Box sx={{display: 'flex'}}>
+                    <Box 
+                        component="main"
+                        sx={{
+                            flexGrow: 1,
+                            p: 3,
+                            backgroundColor: colors.grey['100'],
+                            width: 'max-content'
+                        }}
+                    >
+                        <Toolbar />
+                        <Outlet />
+                    </Box>
+                </Box>
+            </Box>
+        )
+
     )
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,51 @@
 import { LoadingButton } from '@mui/lab'
 import { Box, Card, FormControl, TextField, Typography } from '@mui/material'
-import React from 'react'
-import bgImage from '../assets/login-bg.png'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import bgImage from '../assets/login-bg.jpg'
 
 const Login = () => {
+
+    const navigate = useNavigate()
+
+    const [loginErr, setLoginErr] = useState()
+    const [username, setUsername] = useState('')
+    const [usernameErr, setUsernameErr] = useState(false)
+    const [password, setPassword] = useState('')
+    const [passwordErr, setPasswordErr] = useState(false)
+
+    const [onSubmit, setOnSubmit] = useState(false)
+
+    const loginSubmit = async () => {
+        if (onSubmit) return
+        setLoginErr(undefined)
+
+        const checkErr = {
+            username: username.trim().length === 0,
+            password: password.trim().length === 0
+        }
+        setUsernameErr(checkErr.username)
+        setPasswordErr(checkErr.password)
+        if (checkErr.username || checkErr.password) return
+
+        const params = {
+            username,
+            password
+        }
+        setOnSubmit(true)
+        try {
+            const res = await authApi.login(params)
+            localStorage.setItem('token', res.token)
+            setOnSubmit(false)
+            navigate('/')
+        } catch(err) {
+            if (err.response.status === 401) {
+                setLoginErr('success')
+            }
+            setOnSubmit(false)
+        }
+    }
+
     return (
         <Box
             sx={{
@@ -40,7 +82,7 @@ const Login = () => {
                         textAlign='center'
                         mb='4rem'
                         fontWeight='700'
-                        color='secondary'
+                        color='#f50057'
                     >
                         LOGIN SYSTEM
                     </Typography>
@@ -49,9 +91,9 @@ const Login = () => {
                         <TextField
                             label='Username'
                             variant='outlined'
-                            // value={username}
-                            // onChange={(e) => setUsername(e.target.value)}
-                            // error={usernameErr}
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            error={usernameErr}
                         />
                     </FormControl>
                     <FormControl fullWidth>
@@ -59,18 +101,26 @@ const Login = () => {
                             label='Password'
                             type='password'
                             variant='outlined'
-                            // value={password}
-                            // onChange={(e) => setPassword(e.target.value)}
-                            // error={passwordErr}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            error={passwordErr}
                         />
                     </FormControl>
+                    
+                    {
+                        loginErr && <FormControl>
+                            <Typography color="error">
+                                {loginErr}
+                            </Typography>
+                        </FormControl>
+                    }
 
                     <LoadingButton
                         variant='contained'
                         fullWidth
                         size='large'
                         sx={{ marginTop: '1rem' }}
-                        // onClick={loginSubmit}
+                        onClick={loginSubmit}
                     >
                         Sign in
                     </LoadingButton>
